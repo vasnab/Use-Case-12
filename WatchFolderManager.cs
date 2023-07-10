1f35818447247dda2d6505663f66fb970812e14a
Vasyl Nabochenko <vasyl@bankjoy.com>
2023-07-10 12:51:49 +0300

    UC#12 Create documentation

---
+++ WatchFolderManager.cs
@@ -0,0 +1,200 @@
+﻿#region License Information (GPL v3)
+
+/*
+    ShareX - A program that allows you to take screenshots and share any file type
+    Copyright (c) 2007-2023 ShareX Team
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+
+#endregion License Information (GPL v3)
+
+using ShareX.HelpersLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShareX
+{
+    /// <summary>
+    /// Class <c>WatchFolderManager</c> holds list of <c>WatchFolder</c> entities and manages CRUD operations on them
+    /// </summary>
+    public class WatchFolderManager : IDisposable
+    {
+        /// <summary>
+        /// List of <c>WatchFolder</c> entities
+        /// </summary>
+        public List<WatchFolder> WatchFolders { get; private set; }
+
+        /// <summary>
+        /// Method <c>UpdateWatchFolders</c> updates list of watch folders,
+        /// first disposes existent watch folders, next creates new according to task default settings or hotkey settings
+        /// </summary>
+        public void UpdateWatchFolders()
+        {
+            if (WatchFolders != null)
+            {
+                UnregisterAllWatchFolders();
+            }
+
+            WatchFolders = new List<WatchFolder>();
+
+            foreach (WatchFolderSettings defaultWatchFolderSetting in Program.DefaultTaskSettings.WatchFolderList)
+            {
+                AddWatchFolder(defaultWatchFolderSetting, Program.DefaultTaskSettings);
+            }
+
+            foreach (HotkeySettings hotkeySetting in Program.HotkeysConfig.Hotkeys)
+            {
+                foreach (WatchFolderSettings watchFolderSetting in hotkeySetting.TaskSettings.WatchFolderList)
+                {
+                    AddWatchFolder(watchFolderSetting, hotkeySetting.TaskSettings);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns first <c>WatchFolder</c> from collection if matches <c>WatchFolderSettings</c> input, if not found returns null
+        /// </summary>
+        /// <param name="watchFolderSetting"></param>
+        /// <returns><c>WatchFolder</c> or null if not found</returns>
+        private WatchFolder FindWatchFolder(WatchFolderSettings watchFolderSetting)
+        {
+            return WatchFolders.FirstOrDefault(watchFolder => watchFolder.Settings == watchFolderSetting);
+        }
+
+        /// <summary>
+        /// Returns true if <c>WatchFolder</c> with provided <c>WatchFolderSettings</c> already exists, false if not
+        /// </summary>
+        /// <param name="watchFolderSetting"></param>
+        /// <returns>true or false</returns>
+        private bool IsExist(WatchFolderSettings watchFolderSetting)
+        {
+            return FindWatchFolder(watchFolderSetting) != null;
+        }
+
+
+        /// <summary>
+        /// If <c>WatchFolder</c> with provided <c>WatchFolderSettings</c> does not exists,
+        /// will create new <c>WatchFolder</c> with provided settings,
+        /// if MoveFilesToScreenshotsFolder is true will move files to screenshot folder from task settings,
+        /// if WatchFolderEnabled will call Enable method on <c>WatchFolder</c>
+        /// </summary>
+        /// <param name="watchFolderSetting"></param>
+        /// <param name="taskSettings"></param>
+        public void AddWatchFolder(WatchFolderSettings watchFolderSetting, TaskSettings taskSettings)
+        {
+            if (!IsExist(watchFolderSetting))
+            {
+                if (!taskSettings.WatchFolderList.Contains(watchFolderSetting))
+                {
+                    taskSettings.WatchFolderList.Add(watchFolderSetting);
+                }
+
+                WatchFolder watchFolder = new WatchFolder();
+                watchFolder.Settings = watchFolderSetting;
+                watchFolder.TaskSettings = taskSettings;
+
+                watchFolder.FileWatcherTrigger += origPath =>
+                {
+                    TaskSettings taskSettingsCopy = TaskSettings.GetSafeTaskSettings(taskSettings);
+                    string destPath = origPath;
+
+                    if (watchFolderSetting.MoveFilesToScreenshotsFolder)
+                    {
+                        string screenshotsFolder = TaskHelpers.GetScreenshotsFolder(taskSettingsCopy);
+                        string fileName = Path.GetFileName(origPath);
+                        destPath = TaskHelpers.HandleExistsFile(screenshotsFolder, fileName, taskSettingsCopy);
+                        FileHelpers.CreateDirectoryFromFilePath(destPath);
+                        File.Move(origPath, destPath);
+                    }
+
+                    UploadManager.UploadFile(destPath, taskSettingsCopy);
+                };
+
+                WatchFolders.Add(watchFolder);
+
+                if (taskSettings.WatchFolderEnabled)
+                {
+                    watchFolder.Enable();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes <c>WatchFolder</c> by provided <c>WatchFolderSettings</c> if it exists in the list WatchFolders
+        /// </summary>
+        /// <param name="watchFolderSetting"></param>
+        public void RemoveWatchFolder(WatchFolderSettings watchFolderSetting)
+        {
+            using (WatchFolder watchFolder = FindWatchFolder(watchFolderSetting))
+            {
+                if (watchFolder != null)
+                {
+                    watchFolder.TaskSettings.WatchFolderList.Remove(watchFolderSetting);
+                    WatchFolders.Remove(watchFolder);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Enables <c>WatchFolder</c> if found and WatchFolderEnabled is true, otherwise disposes
+        /// </summary>
+        /// <param name="watchFolderSetting"></param>
+        public void UpdateWatchFolderState(WatchFolderSettings watchFolderSetting)
+        {
+            WatchFolder watchFolder = FindWatchFolder(watchFolderSetting);
+            if (watchFolder != null)
+            {
+                if (watchFolder.TaskSettings.WatchFolderEnabled)
+                {
+                    watchFolder.Enable();
+                }
+                else
+                {
+                    watchFolder.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposes all <c>WatchFolder</c> in WatchFolders list
+        /// </summary>
+        public void UnregisterAllWatchFolders()
+        {
+            if (WatchFolders != null)
+            {
+                foreach (WatchFolder watchFolder in WatchFolders)
+                {
+                    if (watchFolder != null)
+                    {
+                        watchFolder.Dispose();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Runs disposal for all <c>WatchFolder</c> entities in the WatchFolders list
+        /// </summary>
+        public void Dispose()
+        {
+            UnregisterAllWatchFolders();
+        }
+    }
+}
